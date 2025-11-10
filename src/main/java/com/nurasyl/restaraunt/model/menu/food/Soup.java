6113df0cea5b0f39c0be89b33784e1e6115e7a54
Kovernikov Vladislav <vlad.kovernikov06@gmail.com>
2025-11-10 16:17:53 +0500

    features(factory, decorator)

---
+++ src/main/java/com/nurasyl/restaraunt/model/menu/food/Soup.java
@@ -0,0 +1,18 @@
+package com.nurasyl.restaraunt.model.menu.food;
+
+import com.nurasyl.restaraunt.factory.menu.MenuItem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Soup implements MenuItem {
+    private String name;
+    private int price;
+    private String description;
+
+    @Override
+    public String getCategory() {
+        return "Soups";
+    }
+}
